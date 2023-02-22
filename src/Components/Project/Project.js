449738d2f5a4c4d64dd727ed08e73365cff88a37
AndrewMuraguri <awakabs@gmail.com>
2023-02-22 17:07:57 +0100

    header white, live link

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -17,7 +17,7 @@ export function Project() {
       </p>
       <div class="user">
       <a href='https://github.com/AndrewMuraguri/robofriends'><button class="src"><img src={require('../media/icons8-source-code-50.png')} alt='code'/></button></a>
-        <button class="live"><img src={require('../media/icons8-website-50.png')} alt='website'></img></button>
+      <a href='https://andrewmuraguri.github.io/RoboFriend/'><button class="live"><img src={require('../media/icons8-website-50.png')} alt='website'></img></button></a>
         <small>10-12-2022</small>
       </div>
     </div>
@@ -36,8 +36,8 @@ export function Project() {
       This project tested both my technical skills in React and problem-solving skill.
       </p>
       <div class="user">
-          <a href='https://github.com/AndrewMuraguri/Portfolio'><button class="src"><img src={require('../media/icons8-source-code-50.png')} alt='source'/></button></a>
-          <button class="live"><img src={require('../media/icons8-website-50.png')} alt='port'></img></button>
+        <a href='https://github.com/AndrewMuraguri/Portfolio'><button class="src"><img src={require('../media/icons8-source-code-50.png')} alt='source'/></button></a>
+        <a href='https://andrewmuraguri.github.io/'><button class="live"><img src={require('../media/icons8-website-50.png')} alt='port'></img></button></a>
           <small>22-02-2023</small>
       </div>
     </div>
